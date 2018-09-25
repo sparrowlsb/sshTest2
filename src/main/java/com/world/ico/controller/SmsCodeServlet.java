ed0513201910c7bf2b9d26649a2df16105fe9506
SparrowFA <1158362548@qq.com>
2018-09-25 21:56:42 +0800

    vercode

--- src/main/java/com/world/ico/controller/SmsCodeServlet.java
+++ src/main/java/com/world/ico/controller/SmsCodeServlet.java
@@ -1,21 +1,18 @@
 package com.world.ico.controller;
 
 import com.world.ico.service.SmsService;
+import com.world.ico.util.VerifyCodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Random;
 
 /**
  * Created by lsb on 2018/9/19.
@@ -33,43 +30,23 @@ public class SmsCodeServlet {
 
     @RequestMapping(value = "/VerifyCode", produces = "application/json;charset=utf-8", method = RequestMethod.POST)
     @ResponseBody
-    public void VerifyCode(HttpServletRequest request, HttpServletResponse response,HttpSession session) {
-        // 创建一个宽100,高50,且不带透明色的image对象 100 50
-        BufferedImage bi = new BufferedImage(100, 50, BufferedImage.TYPE_INT_RGB);
-        Graphics g = bi.getGraphics();
-        //RGB色彩
-        Color c = new Color(200, 150, 255);
-        // 框中的背景色
-        g.setColor(c);
-        // 颜色填充像素
-        g.fillRect(0, 0, 100, 50);
+    public void VerifyCode(HttpServletRequest request, HttpServletResponse response,HttpSession session) throws IOException {
+            response.setHeader("Pragma", "No-cache");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setContentType("image/jpeg");
 
-        // 定义验证码字符数组
-        char[] ch = "ABCDEFGHIJKLMNPQRSTUVWXYZ0123456798".toCharArray();
-        Random r = new Random();
-        int len = ch.length;
-        int index;
-        StringBuffer sb = new StringBuffer();
-        // 取出四个数字
-        for (int i = 0; i < 4; i++) {
-            // 循环四次随机取长度定义为索引
-            index = r.nextInt(len);
-            g.setColor(new Color(r.nextInt(88), r.nextInt(188), r.nextInt(255)));
-            Font font = new Font("Times New Roman", Font.ITALIC, 18);
-            g.setFont(font);
-            g.drawString(ch[index] + "", (i * 18) + 10, 30);
-            sb.append(ch[index]);
-        }
-        //放入Session中
+            //生成随机字串
+            String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+            //存入会话session
 
-        System.out.print(sb.toString());
-        session.setAttribute("piccode", sb.toString());
-        try {
-            ImageIO.write(bi, "JPG", response.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
+            //删除以前的
+            session.removeAttribute("verCode");
+            session.setAttribute("verCode", verifyCode.toLowerCase());
+            //生成图片
+            int w = 100, h = 30;
+            VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
+
         }
+
     }
-
-
-}
