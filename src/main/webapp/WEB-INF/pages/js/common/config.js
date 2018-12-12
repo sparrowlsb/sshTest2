2c7be2c3fed7881f48e4e3c3a41b023f13e68fab
SparrowFA <1158362548@qq.com>
2018-12-12 21:05:15 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/world/ico/service/serviceImpl/FundServiceImpl.java
    #	src/main/webapp/WEB-INF/pages/js/common/config.js

--- src/main/webapp/WEB-INF/pages/js/common/config.js
+++ src/main/webapp/WEB-INF/pages/js/common/config.js
@@ -1,6 +1,6 @@
 var config = {
-    // api_prefix: "http://www.zhongzhuang.fund/",
-    api_prefix: "http://localhost:8080/",
+    api_prefix: "http://www.zhongzhuang.fund/",
+    // api_prefix: "http://localhost:8080/",
     api_getVerifyImg: "sms/VerifyCode",
     api_sendEmailVerify: "sms/VerifyEmailCode",
     api_regist: "login/registerUser",
