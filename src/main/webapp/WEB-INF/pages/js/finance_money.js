d59e1eb5870019d12a9d6770f18f9392b27eca01
SparrowFA <1158362548@qq.com>
2018-12-15 17:40:13 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/world/ico/service/serviceImpl/FundServiceImpl.java
    #	src/main/webapp/WEB-INF/pages/js/common/config.js

--- src/main/webapp/WEB-INF/pages/js/finance_money.js
+++ src/main/webapp/WEB-INF/pages/js/finance_money.js
@@ -35,7 +35,6 @@ var charge = new Vue({
                 contentType: "application/json;charset=utf-8",
                 url: "/curb/getHist?page="+page,
                 success: function (data, textStatus) {
-                    console.log(data.data)
                     if (data.result == 1) {
 
                         self.charges = data.data.transactionHist;
@@ -57,7 +56,7 @@ var myWallet = new Vue({
     el: '#myWallet',
 
     data: {
-        wallets:[{type:"请先登录",money:"0"},{type:"请先登录",money:"0"},{type:"请先登录",money:"0"},{type:"请先登录",money:"0"}],
+        wallets: []
     },
     methods: {
         ajaxData: function () {
@@ -79,6 +78,7 @@ var myWallet = new Vue({
         this.ajaxData()
     }
 })
+
 var tradeRecord = new Vue({
     el: '#tradeRecord',
     data: {
@@ -156,7 +156,22 @@ var main = new Vue({
         USDT:{type:"usdt",money:"0"},
     },
     methods: {
-
+        getDailyPrice: function (fundId) {
+            var self = this;
+            $.ajax({
+                type: 'GET',
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                url: "/fund/fundDailyPrice",
+                success: function (data, textStatus) {
+                    if (data.result == 1){
+                        self.fund = data.data.fund1
+                        self.fund2 = data.data.fund2
+                        self.fund3 = data.data.fund3
+                    }
+                }
+            });
+        },
         getMaxBuy: function (fundId) {
             var self = this;
             $.ajax({
@@ -193,7 +208,6 @@ var main = new Vue({
                 contentType: "application/json;charset=utf-8",
                 url: "/curb/getHist",
                 success: function (data, textStatus) {
-                    console.log(data.data)
                     if (data.result == 1) {
                         self.charges = data.data.transactionHist;
                     }
@@ -214,7 +228,7 @@ var main = new Vue({
                 data: JSON.stringify({"sellMoney": money}),
 
                 error: function (request) {  //失败的话
-                    alert("请先注册登录账户！");
+                    alert("Connection error");
                     sellUSDT
                 },
                 success: function (data) {  //成功
@@ -244,7 +258,7 @@ var main = new Vue({
                 data: JSON.stringify({"sellMoney": money}),
 
                 error: function (request) {  //失败的话
-                    alert("请先注册登录账户！");
+                    alert("Connection error");
                     sellUSDT
                 },
                 success: function (data) {  //成功
@@ -270,7 +284,6 @@ var main = new Vue({
                 success: function (data, textStatus) {
                     if (data.result == 1) {
                         self.wallets = data.data.fundsDetails;
-
                         self.USDT = data.data.fundsDetails[0];
 
                     }
@@ -285,39 +298,38 @@ var main = new Vue({
         this.getWallets();
     }
 })
-
 var buyDataSet=null;
 $.ajax({
     type: 'GET',
     dataType: "json",
     contentType: "application/json;charset=utf-8",
-    url: "/curb/getHist",
+    url: "/fund/getDailyBuyHistory",
 
     success: function (data, textStatus) {
-        buyDataSet=data.data.transactionHist;
-        console.log(buyDataSet)
+        buyDataSet=data.data;
         $('#buytable').DataTable( {
             searching:false,
-            'order' : [5,'desc'],
+            'order' : [6,'desc'],
             data: buyDataSet,
             columns: [
-                {title: "订单号",data:"id"},
-                {
-                    title: "类型",
-                    data:"exchangeType",
-                    render: function(data,type,row,meta){
-                        if (data=="提现"){
-                            return '<span style="color:red">'+data+'</span>';
-                        }else {
-                            return '<span style="color:green">'+data+'</span>';
-                        }
-                    }
-                },
-                {title: "交易方式",data:"exchangePlatform"},
-                {title: "交易金额",data:"money"},
-                {title: "订单状态",data:"status"},
-                {title: "交易时间",data:"exchangeDate"}
+                {title: "订单号"},
+                {title: "类型"},
+                {title: "基金编号"},
+                {title: "基金名称"},
+                {title: "订单金额(USDT)"},
+                {title: "订单状态"},
+                {title: "交易时间"},
+                {title: "操作"}
             ],
+            "columnDefs": [{
+                // 定义操作列
+                "targets": 7,
+                "data": null,
+                "render": function(data, type, row) {
+                    var html = '<a href="javascript:void(0);"  class="delete btn btn-default btn-xs"><i class="fa fa-times"></i> 撤销</a>'
+                    return html;
+                }
+            }],
             language: {
                 "processing": "处理中...",
                 "lengthMenu": "显示 _MENU_ 项结果",
@@ -343,6 +355,130 @@ $.ajax({
                 }
             }
         } );
+// 初始化刪除按钮
+        $('#buytable tbody').on('click', 'a.delete', function(e) {
+            e.preventDefault();
+
+            if (confirm("确定要删除该订单？")) {
+                var table = $('#buytable').DataTable();
+                table.row($(this).parents('tr')).remove().draw();
+                var id = $('td', $(this).parents('tr')).eq(0).text();
+
+
+
+                $.ajax({
+                    type: "POST",
+                    url: "/fund/revokefund",
+                    dataType: "json",
+                    contentType:"application/json;charset=utf-8",
+
+                    data: JSON.stringify({"id": id,"type":"BUY"}),
+                    success: function (data, textStatus) {
+                        if (data.result == 1) {
+                            alert('撤销成功');
+                        }else {
+                            alert('撤销失败');
+                        }
+                    },
+
+                    error:function(data){
+                        alert('撤销失败');
+                    }
+                });
+            }
+
+        });
+    }
+})
+var sellDataSet=null;
+$.ajax({
+    type: 'GET',
+    dataType: "json",
+    contentType: "application/json;charset=utf-8",
+    url: "/fund/getDailySellHistory",
+
+    success: function (data, textStatus) {
+        sellDataSet=data.data;
+        $('#selltable').DataTable( {
+            'order' : [6,'desc'],
+            data: sellDataSet,
+            columns: [
+                {title: "订单号"},
+                {title: "类型"},
+                {title: "基金编号"},
+                {title: "基金名称"},
+                {title: "订单数量"},
+                {title: "订单状态"},
+                {title: "交易时间"},
+                {title: "操作"}
+            ],
+            "columnDefs": [{
+                // 定义操作列
+                "targets": 7,
+                "data": null,
+                "render": function(data, type, row) {
+                    var html = '<a href="javascript:void(0);"  class="delete btn btn-default btn-xs"><i class="fa fa-times"></i> 撤销</a>'
+                    return html;
+                }
+            }],
+            language: {
+                "processing": "处理中...",
+                "lengthMenu": "显示 _MENU_ 项结果",
+                "zeroRecords": "没有匹配结果",
+                "info": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+                "infoEmpty": "显示第 0 至 0 项结果，共 0 项",
+                "infoFiltered": "(由 _MAX_ 项结果过滤)",
+                "infoPostFix": "",
+                "search": "搜索:",
+                "url": "",
+                "emptyTable": "表中数据为空",
+                "loadingRecords": "载入中...",
+                "infoThousands": ",",
+                "paginate": {
+                    "first": "首页",
+                    "previous": "上页",
+                    "next": "下页",
+                    "last": "末页"
+                },
+                "aria": {
+                    "sortAscending": ": 以升序排列此列",
+                    "sortDescending": ": 以降序排列此列"
+                }
+            }
+        } );
+// 初始化刪除按钮
+        $('#selltable tbody').on('click', 'a.delete', function(e) {
+            e.preventDefault();
+
+            if (confirm("确定要撤销该订单？")) {
+                var table = $('#selltable').DataTable();
+                table.row($(this).parents('tr')).remove().draw();
+                var id = $('td', $(this).parents('tr')).eq(0).text();
+
+
+
+                $.ajax({
+                    type: "POST",
+                    url: "/fund/revokefund",
+                    dataType: "json",
+                    contentType:"application/json;charset=utf-8",
+
+                    data: JSON.stringify({"id": id,"type":"SELL"}),
+                    success: function (data, textStatus) {
+                        if (data.result == 1) {
+                            alert('撤销成功');
+                        }else {
+                            alert('撤销失败');
+                        }
+                    },
+
+                    error:function(data){
+                        alert('撤销失败');
+                    }
+                });
+            }
+
+        });
     }
 })
 
